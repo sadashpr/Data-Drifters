15768d671ffbb7a2fe9ec8dcc32e8d1ea402457f
pradeep sadashiv <pradeep.sadashiv@ac3.com.au>
2023-07-20 15:44:24 +1000

    first commit - sample code + all_wheels

---
+++ reward.py
@@ -0,0 +1,29 @@
+def reward_function(params):
+    '''
+    Example of rewarding the agent to follow center line
+    '''
+    
+    # Read input parameters
+    track_width = params['track_width']
+    distance_from_center = params['distance_from_center']
+    all_wheels = params['all_wheels_on_track']
+    
+    # Calculate 3 markers that are at varying distances away from the center line
+    marker_1 = 0.1 * track_width
+    marker_2 = 0.25 * track_width
+    marker_3 = 0.5 * track_width
+    
+    # Give higher reward if the car is closer to center line and vice versa
+    if distance_from_center <= marker_1:
+        reward = 1.0
+    elif distance_from_center <= marker_2:
+        reward = 0.5
+    elif distance_from_center <= marker_3:
+        reward = 0.1
+    else:
+        reward = 1e-3  # likely crashed/ close to off track
+    
+    if all_wheels:
+        reward = reward *2        # double the reward for being on track 
+    
+    return float(reward)
